2e81a0e971e65c8b9d0d0ad34bbaaddfd6f26a10
silascorreia <silas8_silva@hotmail.com>
2025-07-09 08:49:22 -0300

    Correção comentários

--- src/controllers/desmatamentoController.js
+++ src/controllers/desmatamentoController.js
@@ -7,21 +7,21 @@ function listarDados(requisicao, resposta) {
     resposta.json(dados); //Envia os dados como uma resposta JSON para o cliente
 }
 
-//Funçãio para a requisição GET na rota '/desmatamento/regiao/:regiao'
+//Função para a requisição GET na rota '/desmatamento/regiao/:regiao'
 function listarDadosPorRegiao(requisicao, resposta) {
-    const regiao = requisicao.params.regiao; //Estrai o valor do parâmetro 'regiao' da URL da requisição
+    const regiao = requisicao.params.regiao; //Extrai o valor do parâmetro 'regiao' da URL da requisição
     const dados = desmatamentoModel.obterDadosPorRegiao(regiao);  //Chama a função do model para obter os dados por região
     resposta.json(dados); //Envia os dados filtrados por região como resposta JSON
 }
 
 //Função para a requisição GET na rota '/desmatamento/ano/:ano'
 function listarDadosPorAno(requisicao, resposta) {
-    const ano = requisicao.params.ano; //Estrai o valor do parâmetro 'ano' da URL da requisição
+    const ano = requisicao.params.ano; //Extrai o valor do parâmetro 'ano' da URL da requisição
     const dados = desmatamentoModel.obterDadosPorAno(ano); //Chama a função do model para obter os dados por ano
     resposta.json(dados); //Envia os dados filtrados por ano como uma resposta JSON.
 }
 
-//Exporta as funções para que possam ser utilizadas por outros móludos
+//Exporta as funções para que possam ser utilizadas por outros módulos
 module.exports = {
     listarDados,
     listarDadosPorRegiao,
